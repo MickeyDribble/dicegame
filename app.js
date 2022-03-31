5bb38526caae4b2865acaf9acd04c86e6e4d3224
Michael <michaelf6988@gmail.com>
2022-03-31 16:09:57 +0100

    1-player functionality - no css

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const dice = document.getElementById("dice");
 const roll = document.getElementById("roll");
+const score = document.getElementById("score");
 
+let player1 = 0
 
 roll.addEventListener("click", () => {
+    roll.innerHTML = "Roll"
     let player1Roll = Math.ceil(Math.random()*6);
     dice.src = `image/dice-${player1Roll}.png`;
+    player1 += player1Roll;
+    score.innerHTML = player1;
+    if (player1 >= 20){
+        score.innerHTML = `${player1} YOU WIN!`
+        player1 = 0
+        roll.innerHTML = "Play Again?"
+        
+    } else if (player1Roll === 1) {
+        score.innerHTML = `${player1} You LOSE!!`
+        player1 = 0
+        roll.innerHTML = "Play Again?"
+        
+    }
+
 })
